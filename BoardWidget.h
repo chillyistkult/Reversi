e30be0f6df91a94ae850a0c8823268b8dad2426d
Benjamin Schuch <bschuch@gmx.net>
2015-01-18 05:11:56 +0100

    Added boardwidget skeleton

--- BoardWidget.h
+++ BoardWidget.h
@@ -1,18 +1,42 @@
-#ifndef BOARDWIDGET_H
-#define BOARDWIDGET_H
-
 #include <QWidget>
+#include "board.h"
 
 class BoardWidget : public QWidget
 {
     Q_OBJECT
 public:
-    explicit BoardWidget(QWidget *parent = 0);
+    explicit BoardWidget(QSharedPointer<Board> board,
+                                QWidget *parent = 0);
+
+    BoardWidget(QWidget * parent = 0);
+
+    //virtual from QWidget
+    QSize sizeHint() const;
+
+    void setBoard(QSharedPointer<Board> board);
+    QSharedPointer<Board> getBoard() const;
+
+protected:
+    //virtual from QWidget
+    void paintEvent(QPaintEvent *);
+
+    //virtual from QWidget
+    void mouseReleaseEvent(QMouseEvent *);
+
+    //virtual from QWidget
+    void mousePressEvent(QMouseEvent *);
+
 
 signals:
+    void cellClicked(const BoardPosition cell);
 
 public slots:
 
-};
+private:
+    QSharedPointer<Board> board;
+    QPoint lastMousePressPos;
 
-#endif // BOARDWIDGET_H
+    bool showPossibleWhite;
+    bool showPossibleBlack;
+
+};
--- boardwidget.cpp
+++ boardwidget.cpp
@@ -1,6 +1,51 @@
-#include "boardwidget.h"
+#include "BoardWidget.h"
+
+#include <QPainter>
+#include <QMouseEvent>
+#include <QtDebug>
+#include "Minimax.h"
+
+BoardWidget::BoardWidget(QSharedPointer<Board> board,
+                                       QWidget *parent) :
+    QWidget(parent), showPossibleWhite(false), showPossibleBlack(false)
+{
+    this->setBoard(board);
+}
 
 BoardWidget::BoardWidget(QWidget *parent) :
-    QWidget(parent)
+    QWidget(parent), showPossibleWhite(false), showPossibleBlack(false)
 {
+
+}
+
+QSize BoardWidget::sizeHint() const
+{
+    return QSize(800,800);
+}
+
+void BoardWidget::setBoard(QSharedPointer<Board> board)
+{
+    this->board = board;
+    Board * raw = board.data();
+    connect(raw,
+            SIGNAL(moveMade(CELL_STATE,CELL_STATE)),
+            this,
+            SLOT(update()));
+
+    this->update();
+}
+
+QSharedPointer<Board> BoardWidget::getBoard() const
+{
+    return this->board;
+}
+
+void BoardWidget::paintEvent(QPaintEvent *)
+{
+
+}
+
+void BoardWidget::mousePressEvent(QMouseEvent * event)
+{
+    this->lastMousePressPos = event->pos();
 }
